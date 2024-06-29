21c7b248e9250d76cab5ee50f242555b78c8e5f9
Vaishnavi <vaishnavi293yadav@gmail.com>
2024-06-29 12:51:51 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
 
+
 export default function App() {
   return (
     <BrowserRouter>
